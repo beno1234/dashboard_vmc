a6941621b331bf73d1c520f358f23337261f4cb2
beno1234 <benolopesdias@gmail.com>
2023-05-15 10:26:27 -0300

    alterar

--- src/components/PageTitleWrapper/index.tsx
+++ src/components/PageTitleWrapper/index.tsx
@@ -1,7 +1,7 @@
 import { FC, ReactNode } from 'react';
 import PropTypes from 'prop-types';
 import { Box, styled } from '@mui/material';
-
+// sera que pega
 const PageTitle = styled(Box)(
   ({ theme }) => `
         padding: ${theme.spacing(4)};
